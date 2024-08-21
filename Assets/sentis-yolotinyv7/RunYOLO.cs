035d173310c0d38fffb88f941c94ece7088c8d4f
박사신 <twa6262@naver.com>
2024-08-21 21:26:35 +0900

    중복되는 코드 제거하고 / 애니메이션 중복 제거 / 시간에따라 하늘 색이 바뀌는 코드 추가

--- Assets/sentis-yolotinyv7/RunYOLO.cs
+++ Assets/sentis-yolotinyv7/RunYOLO.cs
@@ -3,264 +3,268 @@ using Unity.Sentis;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RunYOLO : MonoBehaviour
+namespace sentis_yolotinyv7
 {
-    public ModelAsset modelAsset;
-
-    public TextAsset labelsAsset;
-
-    public Sprite borderSprite;
-
-    public Texture2D borderTexture;
-
-    public Font font;
-
-    private Model model;
-
-    private IWorker engine;
-
-    private string[] labels;
-
-    private RenderTexture targetRT;
-
-    private const int imageWidth = 640;
-
-    private const int imageHeight = 640;
-
-    private List<GameObject> boxPool = new List<GameObject>();
-
-    public float mindist = float.MaxValue;
-
-    int minIndex = -1;
-
-    private Color[] classColors = new Color[]
+    public class RunYOLO : MonoBehaviour
     {
-        Color.red,
-        Color.green,
-        Color.blue,
-        Color.yellow,
-        Color.magenta,
-        Color.cyan,
-        Color.gray,
-        Color.white,
-        Color.black
-    };
+        public ModelAsset modelAsset;
 
-    public struct BoundingBox
-    {
-        public float centerX;
-        public float centerY;
-        public float width;
-        public float height;
-        public string label;
-        public float confidence;
-    }
+        public TextAsset labelsAsset;
 
-    void Start()
-    {
-        Application.targetFrameRate = 60;
+        public Sprite borderSprite;
 
-        labels = labelsAsset.text.Split('\n');
+        public Texture2D borderTexture;
 
-        model = ModelLoader.Load(modelAsset);
+        public Font font;
 
-        targetRT = new RenderTexture(2560, 1600, 0);
+        private Model model;
 
-        engine = WorkerFactory.CreateWorker(BackendType.GPUCompute, model);
+        private IWorker engine;
 
-        if (borderSprite == null)
+        private string[] labels;
+
+        private RenderTexture targetRT;
+
+        private const int imageWidth = 640;
+
+        private const int imageHeight = 640;
+
+        private List<GameObject> boxPool = new List<GameObject>();
+
+        public float mindist = float.MaxValue;
+
+        int minIndex = -1;
+
+        private Color[] classColors = new Color[]
         {
-            borderSprite = Sprite.Create(borderTexture, new Rect(0, 0, borderTexture.width, borderTexture.height), new Vector2(borderTexture.width / 2, borderTexture.height / 2));
+            Color.red,
+            Color.green,
+            Color.blue,
+            Color.yellow,
+            Color.magenta,
+            Color.cyan,
+            Color.gray,
+            Color.white,
+            Color.black
+        };
+
+        public struct BoundingBox
+        {
+            public float centerX;
+            public float centerY;
+            public float width;
+            public float height;
+            public string label;
+            public float confidence;
         }
-    }
 
-    private void Update()
-    {
-        ExecuteML();
-
-        if (Input.GetKeyDown(KeyCode.Escape))
+        void Start()
         {
-            Application.Quit();
-        }
-    }
+            Application.targetFrameRate = 60;
 
-    public void ExecuteML()
-    {
-        ClearAnnotations();
+            labels = labelsAsset.text.Split('\n');
 
-        // Camera에서 실시간 화면을 텍스처로 변환하여 모델 입력으로 사용
-        Camera mainCamera = Camera.main;
+            model = ModelLoader.Load(modelAsset);
 
-        if (mainCamera == null) return;
+            targetRT = new RenderTexture(2560, 1600, 0);
 
-        mainCamera.targetTexture = targetRT;
+            engine = WorkerFactory.CreateWorker(BackendType.GPUCompute, model);
 
-        mainCamera.Render();
-
-        mainCamera.targetTexture = null;
-
-        // 텍스처를 모델 입력으로 변환
-        using var input = TextureConverter.ToTensor(targetRT, 640, 640, 3);
-
-        print(input);
-
-        engine.Execute(input);
-
-        var output = engine.PeekOutput() as TensorFloat;
-
-        output.CompleteOperationsAndDownload();
-
-        print(output);
-
-        float displayWidth = Screen.width;
-
-        float displayHeight = Screen.height;
-
-        float scaleX = displayWidth / imageWidth;
-
-        float scaleY = displayHeight / imageHeight;
-
-        int foundBoxes = output.shape[0];
-
-        for (int n = 0; n < foundBoxes; n++)
-        {
-            var box = new BoundingBox
+            if (borderSprite == null)
             {
-                centerX = ((output[n, 1] + output[n, 3]) * 0.5f * scaleX), // 640 * 2560),
-                centerY = displayHeight + ((output[n, 2] + output[n, 4]) * 0.5f * -scaleY),
-                width = (output[n, 3] - output[n, 1]) * scaleX,
-                height = (output[n, 4] - output[n, 2]) * scaleY,
-                label = labels[(int)output[n, 5]],
-                confidence = Mathf.FloorToInt(output[n, 6] * 100 + 0.5f)
-            };
+                borderSprite = Sprite.Create(borderTexture, new Rect(0, 0, borderTexture.width, borderTexture.height), new Vector2(borderTexture.width / 2, borderTexture.height / 2));
+            }
+        }
 
-            /*if(box.confidence > 65)
+        private void Update()
+        {
+            ExecuteML();
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+
+        public void ExecuteML()
+        {
+            ClearAnnotations();
+
+            // Camera에서 실시간 화면을 텍스처로 변환하여 모델 입력으로 사용
+            Camera mainCamera = Camera.main;
+
+            if (mainCamera == null) return;
+
+            mainCamera.targetTexture = targetRT;
+
+            mainCamera.Render();
+
+            mainCamera.targetTexture = null;
+
+            // 텍스처를 모델 입력으로 변환
+            using var input = TextureConverter.ToTensor(targetRT, 640, 640, 3);
+
+            print(input);
+
+            engine.Execute(input);
+
+            var output = engine.PeekOutput() as TensorFloat;
+
+            output.CompleteOperationsAndDownload();
+
+            print(output);
+
+            float displayWidth = Screen.width;
+
+            float displayHeight = Screen.height;
+
+            float scaleX = displayWidth / imageWidth;
+
+            float scaleY = displayHeight / imageHeight;
+
+            int foundBoxes = output.shape[0];
+
+            for (int n = 0; n < foundBoxes; n++)
+            {
+                var box = new BoundingBox
+                {
+                    centerX = ((output[n, 1] + output[n, 3]) * 0.5f * scaleX), // 640 * 2560),
+                    centerY = displayHeight + ((output[n, 2] + output[n, 4]) * 0.5f * -scaleY),
+                    width = (output[n, 3] - output[n, 1]) * scaleX,
+                    height = (output[n, 4] - output[n, 2]) * scaleY,
+                    label = labels[(int)output[n, 5]],
+                    confidence = Mathf.FloorToInt(output[n, 6] * 100 + 0.5f)
+                };
+
+                /*if(box.confidence > 65)
             {
                 DrawBox(box, n);
             }*/
-            DrawBox(box, n);
-        }
-    }
-
-    public void DrawBox(BoundingBox box, int id)
-    {
-        Color boxColor = classColors[id % classColors.Length];
-
-        GameObject panel;
-
-        GameObject[] person = GameObject.FindGameObjectsWithTag("Person");
-
-        print(person.Length);
-
-        if(person.Length > 0) 
-        {
-            float[] distance = new float[person.Length];
-
-            for (int i = 0; i < person.Length; i++) 
-            {
-                distance[i] = Vector3.Distance(this.gameObject.transform.position, person[i].transform.position); 
-
-                print(distance[i]);
-
-                mindist = Mathf.Min(mindist, distance[i]);
+                DrawBox(box, n);
             }
-            print(mindist);
         }
 
-        if (id < boxPool.Count)
+        public void DrawBox(BoundingBox box, int id)
         {
-            panel = boxPool[id];
+            Color boxColor = classColors[id % classColors.Length];
 
-            panel.SetActive(true);
+            GameObject panel;
+
+            GameObject[] person = GameObject.FindGameObjectsWithTag("Person");
+
+            print(person.Length);
+
+            if(person.Length > 0) 
+            {
+                float[] distance = new float[person.Length];
+
+                for (int i = 0; i < person.Length; i++) 
+                {
+                    distance[i] = Vector3.Distance(this.gameObject.transform.position, person[i].transform.position); 
+
+                    print(distance[i]);
+
+                    mindist = Mathf.Min(mindist, distance[i]);
+                }
+                print(mindist);
+            }
+
+            if (id < boxPool.Count)
+            {
+                panel = boxPool[id];
+
+                panel.SetActive(true);
+            }
+            else
+            {
+                panel = CreateNewBox(boxColor);
+            }
+
+            //panel.transform.position = new Vector3(box.centerX, box.centerY);
+
+            panel.transform.position = new Vector3(box.centerX, box.centerY);
+
+            RectTransform rt = panel.GetComponent<RectTransform>();
+
+            rt.sizeDelta = new Vector2(box.width, box.height);
+
+            var label = panel.GetComponentInChildren<Text>();
+
+            label.text = box.label + " (" + box.confidence + "%)";
         }
-        else
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        public GameObject CreateNewBox(Color color)
         {
-            panel = CreateNewBox(boxColor);
+            var panel = new GameObject("ObjectBox");
+
+            panel.AddComponent<CanvasRenderer>();
+
+            Image img = panel.AddComponent<Image>();
+
+            img.color = color;
+
+            img.sprite = borderSprite;
+
+            img.type = Image.Type.Sliced;
+
+            // 적절한 부모 설정 (Canvas 또는 특정 UI 요소)
+            Canvas canvas = FindObjectOfType<Canvas>();
+
+            if (canvas != null)
+            {
+                panel.transform.SetParent(canvas.transform, false);
+            }
+            else
+            {
+                Debug.LogError("Canvas가 씬에 없습니다. Canvas를 추가해 주세요.");
+
+                return null;
+            }
+
+            // 라벨 텍스트 생성 및 설정
+            var text = new GameObject("ObjectLabel");
+
+            text.AddComponent<CanvasRenderer>();
+
+            text.transform.SetParent(panel.transform, false);
+
+            Text txt = text.AddComponent<Text>();
+
+            txt.font = font;
+
+            txt.color = color;
+
+            txt.fontSize = 40;
+
+            txt.horizontalOverflow = HorizontalWrapMode.Overflow;
+
+            RectTransform rt2 = text.GetComponent<RectTransform>();
+
+            rt2.offsetMin = new Vector2(20, 0);
+
+            rt2.offsetMax = new Vector2(-20, 30);
+
+            rt2.anchorMin = new Vector2(0, 0);
+
+            rt2.anchorMax = new Vector2(1, 1);
+
+            boxPool.Add(panel);
+
+            return panel;
         }
 
-        //panel.transform.position = new Vector3(box.centerX, box.centerY);
-
-        panel.transform.position = new Vector3(box.centerX, box.centerY);
-
-        RectTransform rt = panel.GetComponent<RectTransform>();
-
-        rt.sizeDelta = new Vector2(box.width, box.height);
-
-        var label = panel.GetComponentInChildren<Text>();
-
-        label.text = box.label + " (" + box.confidence + "%)";
-    }
-
-    public GameObject CreateNewBox(Color color)
-    {
-        var panel = new GameObject("ObjectBox");
-
-        panel.AddComponent<CanvasRenderer>();
-
-        Image img = panel.AddComponent<Image>();
-
-        img.color = color;
-
-        img.sprite = borderSprite;
-
-        img.type = Image.Type.Sliced;
-
-        // 적절한 부모 설정 (Canvas 또는 특정 UI 요소)
-        Canvas canvas = FindObjectOfType<Canvas>();
-
-        if (canvas != null)
+        public void ClearAnnotations()
         {
-            panel.transform.SetParent(canvas.transform, false);
+            foreach (var box in boxPool)
+            {
+                box.SetActive(false);
+            }
         }
-        else
+
+        private void OnDestroy()
         {
-            Debug.LogError("Canvas가 씬에 없습니다. Canvas를 추가해 주세요.");
-
-            return null;
+            engine?.Dispose();
         }
-
-        // 라벨 텍스트 생성 및 설정
-        var text = new GameObject("ObjectLabel");
-
-        text.AddComponent<CanvasRenderer>();
-
-        text.transform.SetParent(panel.transform, false);
-
-        Text txt = text.AddComponent<Text>();
-
-        txt.font = font;
-
-        txt.color = color;
-
-        txt.fontSize = 40;
-
-        txt.horizontalOverflow = HorizontalWrapMode.Overflow;
-
-        RectTransform rt2 = text.GetComponent<RectTransform>();
-
-        rt2.offsetMin = new Vector2(20, 0);
-
-        rt2.offsetMax = new Vector2(-20, 30);
-
-        rt2.anchorMin = new Vector2(0, 0);
-
-        rt2.anchorMax = new Vector2(1, 1);
-
-        boxPool.Add(panel);
-
-        return panel;
-    }
-
-    public void ClearAnnotations()
-    {
-        foreach (var box in boxPool)
-        {
-            box.SetActive(false);
-        }
-    }
-
-    private void OnDestroy()
-    {
-        engine?.Dispose();
     }
 }
